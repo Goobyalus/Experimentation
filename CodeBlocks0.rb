221f8aa43d960d018f612588ba66c2176ca4ffc7
Goobyalus <jspurch@gmail.com>
2013-09-04 11:39:07 -0400

    ruby code blocks

--- CodeBlocks0.rb
+++ CodeBlocks0.rb
@@ -28,6 +28,51 @@ cond_block()
 '''
 
 #code block with parameters (also conditional)
+'''
 def param_block()
 	yield 1, "2" if block_given?
 end
+
+param_block()
+param_block(){|a,b| puts "no param"}
+#no param
+param_block(){|a,b| puts "#{a}"}
+#1
+param_block(){|a,b| puts "#{a}, #{b}"}
+#1, 2
+param_block(){|a,b,c| puts "#{a}, #{b}"}
+#1, 2
+param_block(){|a,b,c| puts "#{a}, #{b}, #{c}"}
+#1, 2,
+#//note - default is nil
+#//note also that values will be passed in an array and dealt with accordingly
+'''
+
+#code block with variable # or param
+	#Proc.arity --> # param
+'''
+def vararg_block(&block)
+	case block.arity
+		when 0 
+			yield
+		when 1 
+			yield '1'
+		when 2 
+			yield '2','3'
+		else 
+			yield
+	end if block_given?
+end
+vararg_block()
+vararg_block(){puts "no param"}
+vararg_block(){|a| puts "#{a}"}
+vararg_block(){|a,b| puts "#{a}, #{b}"}
+vararg_block(){|a,b,c| puts "#{a}, #{b}, #{c}"}
+#no param
+#1
+#2, 3
+#, ,
+'''
+
+
+
